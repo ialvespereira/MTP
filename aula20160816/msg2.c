dde2ab1c6a192419ca425e335bddb89865980fe5
iago alves <iagoufu@hotmail.com>
2016-08-16 10:21:21 -0300

    final de aula

---
+++ aula20160816/msg2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+if(argc == 1){
+printf("Bom dia ,mundo!!\n");}
+else{
+printf("bom dia, %s!\n",argv[1]);}
+
+return 0;
+}
