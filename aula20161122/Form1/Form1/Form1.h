03d0114ad432dd21601592c6a87784ef260d2b32
Your Name <you@example.com>
2016-11-22 12:02:15 -0200

    VC GUI

--- aula20161122/Form1/Form1/Form1.h
+++ aula20161122/Form1/Form1/Form1.h
@@ -35,16 +35,25 @@ namespace Form1 {
 			}
 		}
 	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::TextBox^  txtUsuario;
 	protected: 
-	private: System::Windows::Forms::TextBox^  textBox1;
+
 	private: System::Windows::Forms::GroupBox^  groupBox1;
-	private: System::Windows::Forms::RadioButton^  radioButton3;
-	private: System::Windows::Forms::RadioButton^  radioButton2;
-	private: System::Windows::Forms::RadioButton^  radioButton1;
-	private: System::Windows::Forms::CheckBox^  checkBox1;
-	private: System::Windows::Forms::CheckBox^  checkBox2;
-	private: System::Windows::Forms::Label^  label1;
-	private: System::Windows::Forms::ComboBox^  comboBox1;
+	private: System::Windows::Forms::RadioButton^  rbdverde;
+
+	private: System::Windows::Forms::RadioButton^  rbdazul;
+	private: System::Windows::Forms::RadioButton^  rbdvermelho;
+	private: System::Windows::Forms::CheckBox^  chknome;
+
+
+
+
+	private: System::Windows::Forms::CheckBox^  chkidade;
+
+	private: System::Windows::Forms::Label^  lblMensagem;
+	private: System::Windows::Forms::ComboBox^  cmbmusicas;
+
+
 	private: System::Windows::Forms::PictureBox^  pictureBox1;
 
 	private:
@@ -62,15 +71,15 @@ namespace Form1 {
 		{
 			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->txtUsuario = (gcnew System::Windows::Forms::TextBox());
 			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
-			this->radioButton1 = (gcnew System::Windows::Forms::RadioButton());
-			this->radioButton2 = (gcnew System::Windows::Forms::RadioButton());
-			this->radioButton3 = (gcnew System::Windows::Forms::RadioButton());
-			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
-			this->checkBox2 = (gcnew System::Windows::Forms::CheckBox());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->rbdverde = (gcnew System::Windows::Forms::RadioButton());
+			this->rbdazul = (gcnew System::Windows::Forms::RadioButton());
+			this->rbdvermelho = (gcnew System::Windows::Forms::RadioButton());
+			this->chknome = (gcnew System::Windows::Forms::CheckBox());
+			this->chkidade = (gcnew System::Windows::Forms::CheckBox());
+			this->lblMensagem = (gcnew System::Windows::Forms::Label());
+			this->cmbmusicas = (gcnew System::Windows::Forms::ComboBox());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->groupBox1->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox1))->BeginInit();
@@ -82,97 +91,104 @@ namespace Form1 {
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(118, 53);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
+			this->button1->Text = L"executar";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
 			// 
-			// textBox1
+			// txtUsuario
 			// 
-			this->textBox1->Location = System::Drawing::Point(12, 37);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(357, 20);
-			this->textBox1->TabIndex = 1;
+			this->txtUsuario->Location = System::Drawing::Point(18, 56);
+			this->txtUsuario->Name = L"txtUsuario";
+			this->txtUsuario->Size = System::Drawing::Size(357, 20);
+			this->txtUsuario->TabIndex = 1;
 			// 
 			// groupBox1
 			// 
-			this->groupBox1->Controls->Add(this->radioButton3);
-			this->groupBox1->Controls->Add(this->radioButton2);
-			this->groupBox1->Controls->Add(this->radioButton1);
+			this->groupBox1->Controls->Add(this->rbdverde);
+			this->groupBox1->Controls->Add(this->rbdazul);
+			this->groupBox1->Controls->Add(this->rbdvermelho);
 			this->groupBox1->Location = System::Drawing::Point(181, 88);
 			this->groupBox1->Name = L"groupBox1";
 			this->groupBox1->Size = System::Drawing::Size(127, 124);
 			this->groupBox1->TabIndex = 3;
 			this->groupBox1->TabStop = false;
-			this->groupBox1->Text = L"groupBox1";
+			this->groupBox1->Text = L"cores";
 			// 
-			// radioButton1
+			// rbdverde
 			// 
-			this->radioButton1->AutoSize = true;
-			this->radioButton1->Location = System::Drawing::Point(28, 37);
-			this->radioButton1->Name = L"radioButton1";
-			this->radioButton1->Size = System::Drawing::Size(85, 17);
-			this->radioButton1->TabIndex = 0;
-			this->radioButton1->TabStop = true;
-			this->radioButton1->Text = L"radioButton1";
-			this->radioButton1->UseVisualStyleBackColor = true;
+			this->rbdverde->AutoSize = true;
+			this->rbdverde->Location = System::Drawing::Point(28, 83);
+			this->rbdverde->Name = L"rbdverde";
+			this->rbdverde->Size = System::Drawing::Size(52, 17);
+			this->rbdverde->TabIndex = 4;
+			this->rbdverde->TabStop = true;
+			this->rbdverde->Text = L"verde";
+			this->rbdverde->UseVisualStyleBackColor = true;
 			// 
-			// radioButton2
+			// rbdazul
 			// 
-			this->radioButton2->AutoSize = true;
-			this->radioButton2->Location = System::Drawing::Point(28, 60);
-			this->radioButton2->Name = L"radioButton2";
-			this->radioButton2->Size = System::Drawing::Size(85, 17);
-			this->radioButton2->TabIndex = 1;
-			this->radioButton2->TabStop = true;
-			this->radioButton2->Text = L"radioButton2";
-			this->radioButton2->UseVisualStyleBackColor = true;
+			this->rbdazul->AutoSize = true;
+			this->rbdazul->Location = System::Drawing::Point(28, 60);
+			this->rbdazul->Name = L"rbdazul";
+			this->rbdazul->Size = System::Drawing::Size(44, 17);
+			this->rbdazul->TabIndex = 1;
+			this->rbdazul->TabStop = true;
+			this->rbdazul->Text = L"azul";
+			this->rbdazul->UseVisualStyleBackColor = true;
 			// 
-			// radioButton3
+			// rbdvermelho
 			// 
-			this->radioButton3->AutoSize = true;
-			this->radioButton3->Location = System::Drawing::Point(28, 83);
-			this->radioButton3->Name = L"radioButton3";
-			this->radioButton3->Size = System::Drawing::Size(85, 17);
-			this->radioButton3->TabIndex = 4;
-			this->radioButton3->TabStop = true;
-			this->radioButton3->Text = L"radioButton3";
-			this->radioButton3->UseVisualStyleBackColor = true;
+			this->rbdvermelho->AutoSize = true;
+			this->rbdvermelho->Location = System::Drawing::Point(28, 37);
+			this->rbdvermelho->Name = L"rbdvermelho";
+			this->rbdvermelho->Size = System::Drawing::Size(68, 17);
+			this->rbdvermelho->TabIndex = 0;
+			this->rbdvermelho->TabStop = true;
+			this->rbdvermelho->Text = L"vermelho";
+			this->rbdvermelho->UseVisualStyleBackColor = true;
 			// 
-			// checkBox1
+			// chknome
 			// 
-			this->checkBox1->AutoSize = true;
-			this->checkBox1->Location = System::Drawing::Point(51, 88);
-			this->checkBox1->Name = L"checkBox1";
-			this->checkBox1->Size = System::Drawing::Size(80, 17);
-			this->checkBox1->TabIndex = 4;
-			this->checkBox1->Text = L"checkBox1";
-			this->checkBox1->UseVisualStyleBackColor = true;
+			this->chknome->AutoSize = true;
+			this->chknome->Location = System::Drawing::Point(51, 88);
+			this->chknome->Name = L"chknome";
+			this->chknome->Size = System::Drawing::Size(52, 17);
+			this->chknome->TabIndex = 4;
+			this->chknome->Text = L"nome";
+			this->chknome->UseVisualStyleBackColor = true;
+			this->chknome->CheckedChanged += gcnew System::EventHandler(this, &Form1::checkBox1_CheckedChanged);
 			// 
-			// checkBox2
+			// chkidade
 			// 
-			this->checkBox2->AutoSize = true;
-			this->checkBox2->Location = System::Drawing::Point(51, 129);
-			this->checkBox2->Name = L"checkBox2";
-			this->checkBox2->Size = System::Drawing::Size(80, 17);
-			this->checkBox2->TabIndex = 5;
-			this->checkBox2->Text = L"checkBox2";
-			this->checkBox2->UseVisualStyleBackColor = true;
+			this->chkidade->AutoSize = true;
+			this->chkidade->Location = System::Drawing::Point(51, 129);
+			this->chkidade->Name = L"chkidade";
+			this->chkidade->Size = System::Drawing::Size(52, 17);
+			this->chkidade->TabIndex = 5;
+			this->chkidade->Text = L"idade";
+			this->chkidade->UseVisualStyleBackColor = true;
 			// 
-			// label1
+			// lblMensagem
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(33, 12);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(35, 13);
-			this->label1->TabIndex = 6;
-			this->label1->Text = L"label1";
+			this->lblMensagem->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->lblMensagem->Location = System::Drawing::Point(12, 9);
+			this->lblMensagem->Name = L"lblMensagem";
+			this->lblMensagem->Size = System::Drawing::Size(930, 32);
+			this->lblMensagem->TabIndex = 6;
+			this->lblMensagem->Text = L"...";
+			this->lblMensagem->TextAlign = System::Drawing::ContentAlignment::TopCenter;
 			// 
-			// comboBox1
+			// cmbmusicas
 			// 
-			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Location = System::Drawing::Point(10, 180);
-			this->comboBox1->Name = L"comboBox1";
-			this->comboBox1->Size = System::Drawing::Size(121, 21);
-			this->comboBox1->TabIndex = 7;
+			this->cmbmusicas->FormattingEnabled = true;
+			this->cmbmusicas->Items->AddRange(gcnew cli::array< System::Object^  >(4) {L"invasible", L"vertigo", L"miss saraievo", L"one love"});
+			this->cmbmusicas->Location = System::Drawing::Point(10, 180);
+			this->cmbmusicas->Name = L"cmbmusicas";
+			this->cmbmusicas->Size = System::Drawing::Size(121, 21);
+			this->cmbmusicas->TabIndex = 7;
+			this->cmbmusicas->Text = L"musicas";
+			this->cmbmusicas->SelectedIndexChanged += gcnew System::EventHandler(this, &Form1::comboBox1_SelectedIndexChanged);
 			// 
 			// pictureBox1
 			// 
@@ -189,15 +205,15 @@ namespace Form1 {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(954, 531);
 			this->Controls->Add(this->pictureBox1);
-			this->Controls->Add(this->comboBox1);
-			this->Controls->Add(this->label1);
-			this->Controls->Add(this->checkBox2);
-			this->Controls->Add(this->checkBox1);
+			this->Controls->Add(this->cmbmusicas);
+			this->Controls->Add(this->lblMensagem);
+			this->Controls->Add(this->chkidade);
+			this->Controls->Add(this->chknome);
 			this->Controls->Add(this->groupBox1);
-			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->txtUsuario);
 			this->Controls->Add(this->button1);
 			this->Name = L"Form1";
-			this->Text = L"Form1";
+			this->Text = L"U2";
 			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
 			this->groupBox1->ResumeLayout(false);
 			this->groupBox1->PerformLayout();
@@ -208,7 +224,31 @@ namespace Form1 {
 		}
 #pragma endregion
 	private: System::Void Form1_Load(System::Object^  sender, System::EventArgs^  e) {
+			 cmbmusicas->SelectedIndex = 0;
 			 }
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		lblMensagem->Text = "A banda , ";
+			if(chknome->Checked)
+				lblMensagem->Text = lblMensagem->Text + "U2, ";
+		if(chkidade->Checked)
+				lblMensagem->Text = lblMensagem->Text + " tem 40 anos ";
+
+		lblMensagem->Text = lblMensagem->Text + "e escreveu " +
+								 cmbmusicas->SelectedItem->ToString() +", ";
+		  lblMensagem->Text = lblMensagem->Text + txtUsuario->Text + ".";
+		  if(rbdvermelho->Checked)
+			  lblMensagem->ForeColor = System ::Drawing::Color::DarkRed;
+		  if(rbdverde->Checked)
+			  lblMensagem->ForeColor = System ::Drawing::Color::ForestGreen;
+		  if(rbdazul->Checked)
+			  lblMensagem->ForeColor = System ::Drawing::Color::MidnightBlue;
+		 }
+private: System::Void checkBox1_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		
+
+		 }
+private: System::Void comboBox1_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
 };
 }
 
